3dee3e6730f614ad455b471c3f7cb690e642bf0f
Lucas Giordano <giordano3102lucas@gmail.com>
2025-01-08 12:17:07 +0100

    reduce number of retries in action listing

--- tests/browser/test_context.py
+++ tests/browser/test_context.py
@@ -73,6 +73,7 @@ def test_subgraph_without_existing_actions(nested_graph: NotteNode, browser_snap
     assert len(context.interaction_nodes()) == 10
     # test with A1
     subgraph = context.subgraph_without([Action(id="A1", description="A1", category="A1")])
+    assert subgraph is not None
     assert subgraph.node.find("A1") is None
     assert len(subgraph.interaction_nodes()) == 9
     # test with A1, A2, A3
@@ -83,6 +84,7 @@ def test_subgraph_without_existing_actions(nested_graph: NotteNode, browser_snap
             Action(id="A3", description="A3", category="A3"),
         ]
     )
+    assert subgraph is not None
     assert subgraph.node.find("A1") is None
     assert subgraph.node.find("A2") is None
     assert subgraph.node.find("A3") is None
@@ -98,6 +100,7 @@ def test_subgraph_without_existing_actions(nested_graph: NotteNode, browser_snap
             Action(id="C2", description="C2", category="C2"),
         ]
     )
+    assert subgraph is not None
     assert subgraph.node.find("B1") is None
     assert subgraph.node.find("B2") is None
     assert subgraph.node.find("C2") is None
