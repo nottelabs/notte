1e50ece2f95b72e7ad16005536cab661f7847b4e
Lucas Giordano <giordano3102lucas@gmail.com>
2025-01-24 17:57:49 +0100

    remove classvar

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,36 +1,36 @@
-import pytest
-from loguru import logger
+# import pytest
+# from loguru import logger
 
-from notte.browser.driver import PlaywrightResource
-
-
-def get_module_name(item: pytest.Item) -> str:
-    return item.module.__name__ if item.module else "unknown_module"
+# from notte.browser.driver import PlaywrightResource
 
 
-_last_module = None
+# def get_module_name(item: pytest.Item) -> str:
+#     return item.module.__name__ if item.module else "unknown_module"
 
 
-@pytest.hookimpl(trylast=True)
-def pytest_runtest_teardown(item: pytest.Item):
-    """Run cleanup after each test and module"""
-    global _last_module
-    current_module = get_module_name(item)
+# _last_module = None
 
-    import asyncio
 
-    # Run cleanup after each test
-    logger.info(f"Running browser pool cleanup after test: {item.name}")
-    asyncio.run(PlaywrightResource.browser_pool.cleanup(force=True))
+# @pytest.hookimpl(trylast=True)
+# def pytest_runtest_teardown(item: pytest.Item):
+#     """Run cleanup after each test and module"""
+#     global _last_module
+#     current_module = get_module_name(item)
 
-    # Additional cleanup when switching modules
-    if _last_module is not None and current_module != _last_module:
-        logger.info(f"Running browser pool cleanup and stop at the end of module: {_last_module}")
+#     import asyncio
 
-        async def module_cleanup():
-            await PlaywrightResource.browser_pool.cleanup(force=True)
-            await PlaywrightResource.browser_pool.stop()
+#     # Run cleanup after each test
+#     logger.info(f"Running browser pool cleanup after test: {item.name}")
+#     asyncio.run(PlaywrightResource.browser_pool.cleanup(force=True))
 
-        asyncio.run(module_cleanup())
+#     # Additional cleanup when switching modules
+#     if _last_module is not None and current_module != _last_module:
+#         logger.info(f"Running browser pool cleanup and stop at the end of module: {_last_module}")
 
-    _last_module = current_module
+#         async def module_cleanup():
+#             await PlaywrightResource.browser_pool.cleanup(force=True)
+#             await PlaywrightResource.browser_pool.stop()
+
+#         asyncio.run(module_cleanup())
+
+#     _last_module = current_module
