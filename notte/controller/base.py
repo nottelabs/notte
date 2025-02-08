980c217cb4c22b503d8f5676c6a6ff212680d612
Lucas Giordano <giordano3102lucas@gmail.com>
2025-02-08 18:52:20 +0100

    fix new tab bug

--- notte/controller/base.py
+++ notte/controller/base.py
@@ -146,16 +146,26 @@ class BrowserController:
         else:
             retval = await self.execute_browser_action(action)
 
-        if len(context.pages) > num_pages:
-            page = context.pages[-1]
-            await page.bring_to_front()
-            await page.wait_for_load_state()
-            self.page = page
-
-        if retval is not None:
+        # add short wait before we check for new tabs to make sure that
+        # the page has time to be created
+        await self.driver.short_wait()
+        if len(context.pages) != num_pages:
+            new_page = context.pages[-1]
+            logger.info(
+                (
+                    f"🪦 Action {action.id} "
+                    f"{('created a new tab' if len(context.pages) > num_pages else 'closed a tab')}"
+                    f". Current tab is now {len(context.pages) -1} with url: {new_page.url}"
+                )
+            )
+            await new_page.bring_to_front()
+            await new_page.wait_for_load_state()
+            self.page = new_page
+        elif retval is not None:
+            # only return snapshot if we didn't switch to a new tab
+            # otherwise, the snapshot is out of date and we need to take a new one
             return retval
 
-        await self.driver.short_wait()
         return await self.driver.snapshot()
 
     async def execute_multiple(self, actions: list[BaseAction]) -> list[BrowserSnapshot]:
