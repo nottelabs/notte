264098fcce566fa034aa5e1c32458c427898967f
Leo <leo@notte.cc>
2025-02-12 13:22:35 +0100

    feat: add more info

--- tests/integration/test_e2e.py
+++ tests/integration/test_e2e.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 import logging
 import os
 import time
@@ -8,8 +9,11 @@ from typing import Any
 
 import pandas as pd
 import pytest
+import tiktoken
 from joblib import Parallel, delayed
+from pydantic import BaseModel, computed_field
 
+from eval.patcher import AgentPatcher
 from eval.webvoyager.load_data import (
     WebVoyagerSubset,
     WebVoyagerTask,
@@ -19,30 +23,108 @@ from examples.simple.agent import SimpleAgent
 from notte.browser.pool import BrowserPool
 
 
+class RunOutput(BaseModel):
+    success: bool
+    answer: str
+    num_steps: int
+    input_tokens: dict[str, list[Any]]
+    output_tokens: dict[str, list[Any]]
+    duration_in_s: float
+
+
+class LLMCall(BaseModel):
+    input_tokens: int
+    output_tokens: int
+    messages_in: list[dict[str, Any]]
+    message_out: dict[str, Any]
+
+
+class TaskResult(BaseModel):
+    success: bool = False
+    duration_in_s: float
+    agent_answer: str
+    task: WebVoyagerTask
+    num_steps: int
+    llm_calls: list[LLMCall]
+
+    @computed_field
+    def task_description(self) -> str:
+        return self.task.question
+
+    @computed_field
+    def task_id(self) -> int:
+        return self.task.id
+
+    @computed_field
+    def task_website(self) -> str:
+        return self.task.name
+
+    @computed_field
+    def reference_answer(self) -> str:
+        return self.task.ref_answers[0].answer
+
+    @computed_field
+    def total_input_tokens(self) -> int:
+        return sum(step.input_tokens for step in self.llm_calls)
+
+    @computed_field
+    def total_output_tokens(self) -> int:
+        return sum(step.output_tokens for step in self.llm_calls)
+
+    @computed_field
+    def last_message(self) -> str:
+        if len(self.llm_calls) == 0:
+            return ""
+
+        return json.dumps(self.llm_calls[-1].message_out)
+
+
 @pytest.fixture(scope="session")
 def agent_llm(pytestconfig):
     return pytestconfig.getoption("agent_llm")
 
 
-def run_agent(browser_pool: BrowserPool, agent_llm: str, task: WebVoyagerTask) -> dict[str, Any]:
+def run_agent(browser_pool: BrowserPool, agent_llm: str, task: WebVoyagerTask) -> tuple[WebVoyagerTask, RunOutput]:
     task_str = f"Your task: {task.question}. Use {task.url or 'the web'} to answer the question."
+    start = time.time()
 
     async def _async_run():
-        start = time.time()
         try:
-            agent = SimpleAgent(model=agent_llm, headless=True, raise_on_failure=False)
+            agent = SimpleAgent(pool=browser_pool, model=agent_llm, headless=True, raise_on_failure=False)
+
+            patcher = AgentPatcher()
+            _ = patcher.log_io(agent.llm, ["completion"])
 
             output = await agent.run(task_str)
-            success = output.success
+
+            return output, patcher
 
         except Exception as e:
             logging.error(f"Error running task: {task}: {e} {traceback.format_exc()}")
-            success = False
 
-        total_time = time.time() - start
-        return {"website": task.name, "id": task.id, "success": success, "time": total_time}
+    output, patcher = asyncio.run(_async_run())
 
-    return asyncio.run(_async_run())
+    if output is not None:
+        success = output.success
+        answer = output.answer
+        num_steps = len(output.trajectory)
+    else:
+        success = False
+        answer = ""
+
+        # assume as many llm calls as there are steps
+        num_steps = len(patcher.input_data["LLMEngine.completion"])
+
+    return task, RunOutput(
+        success=success,
+        answer=answer,
+        num_steps=num_steps,
+        duration_in_s=time.time() - start,
+        input_tokens=patcher.input_data,
+        output_tokens=patcher.output_data,
+    )
+
+    return task, asyncio.run(_async_run())
 
 
 @pytest.mark.timeout(60 * 60 * 2)  # fail after 2 hours
@@ -59,12 +141,76 @@ async def test_benchmark_webvoyager(agent_llm: str, monkeypatch) -> None:
     monkeypatch.setenv("CEREBRAS_API_KEY", api_key)
 
     browser_pool = BrowserPool()
-    results: list[dict[str, Any]] = typing.cast(
-        list[dict[str, Any]],
-        Parallel(n_jobs=2)(delayed(run_agent)(browser_pool, agent_llm, task) for task in tasks),
+    results: list[tuple[WebVoyagerTask, RunOutput | None]] = typing.cast(
+        list[tuple[WebVoyagerTask, RunOutput | None]],
+        Parallel(n_jobs=2)(delayed(run_agent)(browser_pool, agent_llm, task) for task in tasks[:2]),
     )
-    results = sorted(results, key=lambda x: (x["website"], x["id"]))
 
-    df = pd.DataFrame(results)
-    logging.info(f"\n\n{df.to_markdown()}")
-    assert all(result["success"] for result in results)
+    parsed_results = [parse_output(agent_llm, task, run_output) for task, run_output in results]
+
+    df = pd.DataFrame((x.model_dump() for x in parsed_results)).sort_values(by=["task_website", "task_id"])
+    filtered = df[
+        [
+            "task_website",
+            "task_id",
+            "success",
+            "duration_in_s",
+            "num_steps",
+            "total_input_tokens",
+            "total_output_tokens",
+        ]
+    ]
+
+    logging.info(f"\n\n{filtered.to_markdown()}")
+
+    os.makedirs("dist", exist_ok=True)
+
+    filtered.to_markdown(os.path.join("dist", "results.md"))
+    df.to_json(os.path.join("dist", "results.jsonl"), orient="records", lines=True)
+
+    assert df.success.all()
+
+
+def parse_output(agent_key: str, task: WebVoyagerTask, run_output: RunOutput) -> TaskResult:
+    encoding = tiktoken.get_encoding("cl100k_base")
+
+    input_messages = [json.loads(message) for message in run_output.input_tokens["LLMEngine.completion"]]
+    input_tokens = [" ".join(message["content"] for message in step["messages"]) for step in input_messages]
+    num_inputs_per_step = [len(encoding.encode(tokens)) for tokens in input_tokens]
+
+    output_messages = [json.loads(message) for message in run_output.output_tokens["LLMEngine.completion"]]
+    output_tokens = [step["choices"][0]["message"]["content"] for step in output_messages]
+    num_outputs_per_step = [len(encoding.encode(tokens)) for tokens in output_tokens]
+
+    try:
+        agent_answer = run_output.output.answer
+    except Exception:
+        agent_answer = ""
+
+    llm_calls = []
+    for inp_message, out_message, inp_tokens, out_tokens in zip(
+        input_messages, output_messages, num_inputs_per_step, num_outputs_per_step
+    ):
+
+        messages_in = [message for message in inp_message["messages"]]
+        message_out = out_message["choices"][0]["message"]
+
+        llm_calls.append(
+            LLMCall(
+                input_tokens=inp_tokens,
+                output_tokens=out_tokens,
+                messages_in=messages_in,
+                message_out=message_out,
+            )
+        )
+
+    task_res = TaskResult(
+        success=run_output.success,
+        duration_in_s=run_output.duration_in_s,
+        num_steps=run_output.num_steps,
+        agent_answer=agent_answer,
+        task=task,
+        llm_calls=llm_calls,
+    )
+
+    return task_res
