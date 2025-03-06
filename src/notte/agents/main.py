a1e970b13adafd4f4618176b76ba21e2b38f01c7
Lucas Giordano <giordano3102lucas@gmail.com>
2025-03-06 23:30:35 +0100

    rename marcel

--- src/notte/agents/main.py
+++ src/notte/agents/main.py
@@ -1,16 +1,11 @@
 import asyncio
 
-from notte.agents.falco.agent import (
-    FalcoAgent as Agent,
-)
-from notte.agents.falco.agent import (
-    FalcoAgentConfig as AgentConfig,
-)
+from notte.agents.falco.agent import FalcoAgent, FalcoAgentConfig
 from notte.common.agent.types import AgentResponse
 from notte.sdk.types import DEFAULT_MAX_NB_STEPS
 
 
-class MarcelAgent:
+class Agent:
     def __init__(
         self,
         headless: bool = True,
@@ -18,12 +13,12 @@ class MarcelAgent:
         max_steps: int = DEFAULT_MAX_NB_STEPS,
         disable_web_security: bool = True,
     ):
-        self.config: AgentConfig = AgentConfig(reasoning_model=reasoning_model).map_env(
+        self.config: FalcoAgentConfig = FalcoAgentConfig(reasoning_model=reasoning_model).map_env(
             lambda env: env.user_mode().steps(max_steps).headless(headless)
         )
         if disable_web_security:
             self.config = self.config.map_env(lambda env: env.disable_web_security())
 
     def run(self, task: str) -> AgentResponse:
-        agent = Agent(config=self.config)
+        agent = FalcoAgent(config=self.config)
         return asyncio.run(agent.run(task))
