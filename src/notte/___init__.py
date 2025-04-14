445f4261e60cfd859f46a4f3ec3935b3dbb8d907
Lucas Giordano <giordano3102lucas@gmail.com>
2025-04-14 15:54:06 +0200

    fix architecture

---
+++ src/notte/___init__.py
@@ -0,0 +1,6 @@
+from notte_agent.main import Agent
+from notte_browser.env import NotteEnv
+from notte_core import set_error_mode
+from notte_sdk.client import NotteClient
+
+__all__ = ["NotteClient", "NotteEnv", "Agent", "set_error_mode"]
