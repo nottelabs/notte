1143b781d58b344924840e15f865dee3fa1bbc4b
Leo <leo@notte.cc>
2025-05-16 16:34:34 +0200

    get screenshot when failing listing

--- tests/integration/test_resolution.py
+++ tests/integration/test_resolution.py
@@ -1,4 +1,5 @@
 import pytest
+import requests
 from loguru import logger
 from notte_browser.resolution import NodeResolutionPipe
 from notte_browser.session import NotteSession
@@ -52,7 +53,14 @@ async def test_action_node_resolution_pipe(url: str) -> None:
             except Exception as e:
                 errors.append(f"Error for node {node.id}: {e}")
 
-    assert total_count > 0, "No nodes found"
+    if total_count <= 0:
+        if obs.screenshot is not None:
+            url_upload = upload_to_file_io(obs.screenshot)
+        else:
+            url_upload = "but got no screenshot"
+
+        assert total_count > 0, f"No nodes found: {url_upload}"
+
     error_text = "\n".join(errors)
     assert len(error_text) == 0, f"Percentage of errors: {len(errors) / total_count * 100:.2f}%\n Errors:\n{error_text}"
 
