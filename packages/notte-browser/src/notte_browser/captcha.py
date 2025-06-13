5f61135fef573f70cf07e62d12a8322dfe5c03a9
Leo <leo@notte.cc>
2025-06-12 17:15:54 -0700

    handle captcha as pass

--- packages/notte-browser/src/notte_browser/captcha.py
+++ packages/notte-browser/src/notte_browser/captcha.py
@@ -8,14 +8,4 @@ from notte_browser.window import BrowserWindow
 class CaptchaHandler:
     @staticmethod
     async def handle_captchas(window: BrowserWindow, action: CaptchaSolveAction) -> bool:  # pyright: ignore [reportUnusedParameter]
-        _ = await window.page.wait_for_function(
-            """
-    () => {
-        const element = document.querySelector('.antigate_solver');
-        console.log(element.textContent.trim())
-        return element && element.textContent.trim() === 'Solved by notte';
-    }
-""",
-            timeout=0,
-        )
         return True
