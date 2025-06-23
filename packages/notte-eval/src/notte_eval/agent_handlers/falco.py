0d7964b443c41d6e89709be24b557fbb51c24c4d
Lucas Giordano <giordano3102lucas@gmail.com>
2025-06-22 22:01:12 -0700

    cleanup more safe execution

--- packages/notte-eval/src/notte_eval/agent_handlers/falco.py
+++ packages/notte-eval/src/notte_eval/agent_handlers/falco.py
@@ -97,14 +97,13 @@ class FalcoBench(AgentBenchmark[FalcoInput, FalcoOutput]):
         screenshots: list[bytes] = []
         for (step, in_step_calls), hist in zip(out.per_step_calls, out.output.trajectory):
             last_url = ""
-            for res in hist.results:
-                if res.result.success:
-                    obs = res.obs
-                    screen = obs.screenshot
-                    if screen is not None:
-                        screenshots.append(screen)
+            if hist.result.success:
+                obs = hist.obs
+                screen = obs.screenshot
+                if screen is not None:
+                    screenshots.append(screen)
 
-                    last_url = obs.metadata.url
+                last_url = obs.metadata.url
 
             llm_calls: list[LLMCall] = []
             llm_calls_logs = in_step_calls["LLMEngine.completion"]
