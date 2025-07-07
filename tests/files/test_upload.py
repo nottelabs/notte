a839092aced1003c5f9d898c922be7205138bd35
Lucas Giordano <giordano3102lucas@gmail.com>
2025-07-07 14:08:00 -0700

    update test files

--- tests/files/test_upload.py
+++ tests/files/test_upload.py
@@ -1,7 +1,39 @@
+import pytest
 from notte_sdk import NotteClient
+from pydantic import BaseModel
 
 
-def test_uploads(subtests):
+class UploadTest(BaseModel):
+    task: str
+    url: str
+    description: str
+    max_steps: int
+
+
+file_upload_tests = [
+    UploadTest(
+        task="upload cat file, but do not send",
+        url="https://ps.uci.edu/~franklin/doc/file_upload.html",
+        max_steps=3,
+        description="cat_file_upload",
+    ),
+    UploadTest(
+        task="upload cat image",
+        url="https://crop-circle.imageonline.co/",
+        max_steps=3,
+        description="image_upload",
+    ),
+    UploadTest(
+        task="upload the first txt file, do not submit or do anything else",
+        url="https://cloudconvert.com/txt-to-pdf",
+        max_steps=4,
+        description="txt_file_upload",
+    ),
+]
+
+
+@pytest.mark.parametrize("test", file_upload_tests, ids=lambda x: x.description)
+def test_uploads(test: UploadTest):
     notte = NotteClient()
     storage = notte.FileStorage()
 
@@ -9,38 +41,14 @@ def test_uploads(subtests):
         files = ["cat.jpg", "resume.pdf", "text1.txt"]
 
         for f in files:
-            storage.upload(f"tests/files/{f}")
+            resp = storage.upload(f"tests/files/{f}")
+            assert resp.success
 
         uploaded = storage.list(type="uploads")
 
         for f in files:
             assert f in uploaded
 
-        test_cases = [
-            (
-                "upload cat file, but do not send",
-                "https://ps.uci.edu/~franklin/doc/file_upload.html",
-                3,
-                "cat_file_upload",
-            ),
-            ("upload cat image", "https://crop-circle.imageonline.co/", 3, "image_upload"),
-            (
-                "upload the first txt file, do not submit or do anything else",
-                "https://cloudconvert.com/txt-to-pdf",
-                4,
-                "txt_file_upload",
-            ),
-            # Unreliable test:
-            # (
-            #     "just upload the resume, don't do anything else",
-            #     "https://apply.workable.com/huggingface/j/0BD8C06DB3/apply/",
-            #     6,
-            #     "resume_upload",
-            # ),
-        ]
-
-        for task, url, max_steps, description in test_cases:
-            with subtests.test(description=description):
-                agent = notte.Agent(session=session, reasoning_model="gemini/gemini-2.5-flash", max_steps=max_steps)
-                resp = agent.run(url=url, task=task)
-                assert resp.success
+        agent = notte.Agent(session=session, max_steps=test.max_steps)
+        resp = agent.run(url=test.url, task=test.task)
+        assert resp.success
