a81eb4e4732fd0805d9b613d97dd6eb1242a0f22
Leo <leo@notte.cc>
2025-09-08 15:16:56 -0700

    tmp fix

--- packages/notte-browser/src/notte_browser/window.py
+++ packages/notte-browser/src/notte_browser/window.py
@@ -237,11 +237,22 @@ class BrowserWindow(BaseModel):
 
     async def tab_metadata(self, tab_idx: int | None = None) -> TabsData:
         page = self.tabs[tab_idx] if tab_idx is not None else self.page
+        try:
+            page_title = await asyncio.wait_for(page.title(), timeout=1.0)
+        except TimeoutError:
+            page_title = page.url
+
         return TabsData(
             tab_id=tab_idx if tab_idx is not None else -1,
-            title=await page.title(),
+            title=page_title,
             url=page.url,
         )
+        # page = self.tabs[tab_idx] if tab_idx is not None else self.page
+        # return TabsData(
+        #     tab_id=tab_idx if tab_idx is not None else -1,
+        #     title=await page.title(),
+        #     url=page.url,
+        # )
 
     @profiler.profiled()
     async def snapshot_metadata(self) -> SnapshotMetadata:
