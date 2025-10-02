fa739419c1814fa4f002a214e3a2c3cd85bdc76b
Leo <leo@notte.cc>
2025-10-02 08:18:42 -0700

    last try

--- tests/integration/test_resolution.py
+++ tests/integration/test_resolution.py
@@ -49,6 +49,7 @@ async def upload_screenshot_to_0x0(screenshot_bytes: bytes) -> str:
             return screenshot_url.strip()
 
 
+@pytest.mark.asyncio
 @pytest.mark.parametrize(
     "url",
     urls(),
