7d0239b99a99fa4de34bfd423b1d25c2dfe2ca44
Leo <leo@notte.cc>
2026-01-30 11:22:00 +0100

    tmp: vault in sesson

--- src/notte/__init__.py
+++ src/notte/__init__.py
@@ -4,6 +4,16 @@ from notte_browser.session import NotteSession as Session
 from notte_core import check_notte_version, set_error_mode
 from notte_core.common.config import LlmModel as models
 from notte_core.common.config import config
+from notte_core.credentials import (
+    CARD_CVV,
+    CARD_EXPIRATION,
+    CARD_HOLDER_NAME,
+    CARD_NUMBER,
+    EMAIL,
+    MFA,
+    PASSWORD,
+    USERNAME,
+)
 from notte_sdk.client import NotteClient
 from notte_sdk.client import NotteClient as Client
 
@@ -20,4 +30,12 @@ __all__ = [
     "models",
     "config",
     "actions",
+    "EMAIL",
+    "USERNAME",
+    "PASSWORD",
+    "MFA",
+    "CARD_NUMBER",
+    "CARD_HOLDER_NAME",
+    "CARD_EXPIRATION",
+    "CARD_CVV",
 ]
